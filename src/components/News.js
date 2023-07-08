36e6f8f3a8f7235b7967b5bbf1bf27a2f1bc478d
Gagan Tayal <gagantayal2003@gmail.com>
2023-07-08 22:28:02 +0530

    fix

--- src/components/News.js
+++ src/components/News.js
@@ -13,9 +13,10 @@ export class News extends Component {
  async componentDidMount(){
     let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=3dcef5da1ef040f5b5fbb4c17fe4811b"
     let data = await fetch(url);
-    console.log(data)
     let parseData = await data.json()
-    this.setState({article:parseData.article})
+    console.log(parseData)
+    this.setState({article:parseData.articles})
+
  }
 
   render() {
@@ -26,7 +27,7 @@ export class News extends Component {
     <h1>NewsMonkey - top Headlines....</h1>
         <div className='row'>
           {this.state.article.map((element)=>{
-            return <div className='col-md-4 my-3' key={element.url}>
+            return <div className='col-md-3 my-3' key={element.url}>
              <Newsitem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,70):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
             </div>
           })}
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -6,11 +6,11 @@ export class Newsitem extends Component {
     return (
       <div>
           <div className="card" style={{width: "18rem"}}>
-                <img src={imageUrl} className="card-img-top" alt="..."/>
+                <img src={!imageUrl?"https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/ebpqduahzlkecgih_1688732755.jpeg":imageUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
                   <h5 className="card-title">{title}....</h5>
                   <p className="card-text">{description}...</p>
-                  <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Go Read</a>
+                  <a href = {newsUrl} target='_blank' className='btn btn-primary btn-sm'>Read Me</a>
                 </div>
             </div>
       </div>
