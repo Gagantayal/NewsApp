17d6a011e78b01b7872da94bc3054be2ee6224f6
Gagan Tayal <gagantayal2003@gmail.com>
2023-07-09 10:29:08 +0530

    spinner

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem';
+import Spinner from './Spinner';
 
 export class News extends Component {
   constructor(){
@@ -9,39 +10,43 @@ export class News extends Component {
       loading:false,
       page:1
     }
-  }
+  } 
 
  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=3dcef5da1ef040f5b5fbb4c17fe4811b"
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3dcef5da1ef040f5b5fbb4c17fe4811b&pageSize=${this.props.pageSize}`
+    this.setState({loading:true})
     let data = await fetch(url);
     let parseData = await data.json()
     console.log(parseData)
     this.setState({article:parseData.articles, 
-      totalarticle:parseData.totalResults
+      totalarticle:parseData.totalResults,
+      loading:false
     })
 
  }
 
  handleNextClick= async()=>{
-  if(this.state.page+1 > Math.ceil(this.state.totalResults/20)){}
-  else{
-  let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3dcef5da1ef040f5b5fbb4c17fe4811b&page=${this.state.page + 1}&pageSize=20`
+  if(!(this.state.page + 1 > Math.ceil(this.state.totalarticle/this.props.pageSize))){}
+  let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3dcef5da1ef040f5b5fbb4c17fe4811b&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
+  this.setState({loading:true});
   let data = await fetch(url);
   let parseData = await data.json()
-  console.log(parseData)
   this.setState({
     page:this.state.page + 1,
-    article:parseData.articles
-  })}
+    article:parseData.articles,
+    loading:false
+  })
  }
  handlePreviousclick=async()=>{
-  let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3dcef5da1ef040f5b5fbb4c17fe4811b&page=${this.state.page - 1}&pageSize=20`
+  let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3dcef5da1ef040f5b5fbb4c17fe4811b&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
+  this.setState({loading:true});
   let data = await fetch(url);
   let parseData = await data.json()
   console.log(parseData)
   this.setState({
     page:this.state.page - 1,
-    article:parseData.articles
+    article:parseData.articles,
+    loading:false
   })
  }
 
@@ -50,9 +55,10 @@ export class News extends Component {
       <>
 
     <div className='container my-3'>
-    <h1>NewsMonkey - top Headlines....</h1>
+      <h1 className="text-center">NewsMonkey - top Headlines....</h1>
+      {this.state.loading && <Spinner/>}
         <div className='row'>
-          {this.state.article.map((element)=>{
+          {!this.state.loading && this.state.article.map((element)=>{
             return <div className='col-md-3 my-3' key={element.url}>
              <Newsitem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,70):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
             </div>
@@ -60,12 +66,11 @@ export class News extends Component {
       </div>
       <div className="container d-flex justify-content-between">
       <button disabled={this.state.page<=1} type="button" className="btn btn-info"onClick={this.handlePreviousclick}>&larr; Previous</button>
-      <button disabled={this.state.article.newsUrl===null}type="button" className="btn btn-info" onClick={this.handleNextClick}>Next &rarr;</button>
+      <button disabled={this.state.page + 1 > Math.ceil(this.state.totalarticle/this.props.pageSize)}type="button" className="btn btn-info" onClick={this.handleNextClick}>Next &rarr;</button>
       </div>
     </div>
     </>
     )
   }
 }
-
 export default News
---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import loading from './ZZ5H.gif'
+
+export class Spinner extends Component {
+  render() {
+    return (
+      <div className='text-center' >
+            <img src={loading} alt="loading"/>
+      </div>
+    )
+  }
+}
+
+export default Spinner
