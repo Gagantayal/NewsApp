23b38463117b58f95668cd5e60cd8a927fbd0d0a
Gagan Tayal <gagantayal2003@gmail.com>
2023-07-10 11:31:00 +0530

    router

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <BrowserRouter>
         <Navbar/>
-        <News pageSize={16} country="in" category="business"/>
+
+        <Routes>
+        <Route path="/" element={<News key="general" pageSize={16} country="in" category="general"/>} />
+        <Route path="/business" element={<News key="business" pageSize={16} country="in" category="business"/>} />
+        <Route path="/entertainment" element={<News key="entertainment" pageSize={16} country="in" category="entertainment"/>} />
+        <Route path="/health" element={<News key="health" pageSize={16} country="in" category="health"/>} />
+        <Route path="/sports" element={<News key="sports" pageSize={16} country="in" category="sports"/>} />
+        <Route path="/science" element={<News key="science" pageSize={16} country="in" category="science"/>} />
+        <Route path="/technology" element={<News key="technology" pageSize={16} country="in" category="technology"/>} />
+        <Route path="/general" element={<News key="general" pageSize={16} country="in" category="general"/>} />
+      </Routes>
+        </BrowserRouter>
       </div>
     )
   }
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -5,8 +5,8 @@ export class Newsitem extends Component {
     let {title,description,imageUrl,newsUrl} = this.props;
     return (
       <div>
-          <div className="card" style={{width: "18rem" ,height:"25rem"}}>
-                <img src={!imageUrl?"https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/ebpqduahzlkecgih_1688732755.jpeg":imageUrl} style={{width:"18rem", height:"10rem"}} className="card-img-top" alt="..."/>
+          <div className="card">
+                <img src={!imageUrl?"https://img.republicworld.com/republic-prod/stories/promolarge/xhdpi/ebpqduahzlkecgih_1688732755.jpeg":imageUrl} style={{width:"19rem", height:"10rem"}} className="card-img-top" alt="..."/>
                 <div className="card-body">
                   <h5 className="card-title">{title}....</h5>
                   <p className="card-text">{description}...</p>
