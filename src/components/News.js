0e229064a857d3c249270959003c143e800375ad
Gagan Tayal <gagantayal2003@gmail.com>
2024-08-24 20:05:54 +0530

    new updates

--- src/components/News.js
+++ src/components/News.js
@@ -4,8 +4,6 @@ import Spinner from './Spinner';
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-
-
 export class News extends Component {
   static defaultProps = {
     country: 'in',
@@ -13,7 +11,7 @@ export class News extends Component {
     category: 'general'
   }
 
-  static propTypes = {  // Corrected from propsType to propTypes
+  static propTypes = { 
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string,
@@ -57,17 +55,17 @@ export class News extends Component {
     this.updateNews();
   }
 
-  handleNextClick = async () => {
-    this.setState((prevState) => ({
-      page: prevState.page + 1
-    }), this.updateNews);  // Callback ensures updateNews is called after setState
-  }
+  // handleNextClick = async () => {
+  //   this.setState((prevState) => ({
+  //     page: prevState.page + 1
+  //   }), this.updateNews);  // Callback ensures updateNews is called after setState
+  // }
 
-  handlePreviousClick = async () => {
-    this.setState((prevState) => ({
-      page: prevState.page - 1
-    }), this.updateNews);  // Callback ensures updateNews is called after setState
-  }
+  // handlePreviousClick = async () => {
+  //   this.setState((prevState) => ({
+  //     page: prevState.page - 1
+  //   }), this.updateNews);  // Callback ensures updateNews is called after setState
+  // }
 
   fetchMoreData = async () => {
     const { country, category, apiKey, pageSize } = this.props;
@@ -94,77 +92,6 @@ export class News extends Component {
       // Optionally, update the state to show an error message to the user
     }
   };
-  
-
-
-
-
-
-
-
-// export class News extends Component {
-//   static defaultProps = {
-//     country:'in',
-//     pageSize:16,
-//     category:'general'
-//   }
-
-//   static propsType = {
-//     country: PropTypes.string,
-//     pageSize: PropTypes.number,
-//     category: PropTypes.string
-//   }
-
-//   constructor(){
-//     super();
-//     this.state= {
-//       article:[],
-//       loading:true,
-//       page:1
-//     }
-//   } 
-//   updateNews=  async()=>{
-//     this.props.setProgress(0);
-//     this.props.setProgress(20);
-//     console.log(this.props.apiKey)
-//     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page+1}&pageSize=${this.props.pageSize}`
-//     let data = await fetch(url);
-//     this.props.setProgress(40);
-//     let parseData = await data.json()
-//     this.props.setProgress(70);
-//     this.setState({article:parseData.articles, 
-//       totalarticle:parseData.totalResults,
-//       loading:false
-//     })  
-//     this.props.setProgress(100);  
-//   }
-//  async componentDidMount(){
-//   this.updateNews()
-//  }
-
-//  handleNextClick= async()=>{
-//   this.setState({
-//       page:this.state.page + 1
-//  })
-//  this.updateNews()
-// }
-//  handlePreviousclick=async()=>{
-//   this.setState({
-//     page:this.state.page - 1
-//   })
-//   this.updateNews()
-//  }
-//  fetchMoreData = async()=>{
-//    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page+1}&pageSize=${this.props.pageSize}`
-//    this.setState({page:this.state.page + 1})
-//   let data = await fetch(url);
-//   let parseData = await data.json()
-
-//   this.setState({
-//     article:this.state.article.concat(parseData.articles), 
-//     totalarticle:parseData.totalResults
-//   })
-//  }
 
   render() {
     return (
@@ -179,15 +106,7 @@ export class News extends Component {
         >
          <div className="container"> 
         <div className='row'>
-          {/* {this.state.article.map((element)=>{
-            return <div className='col-md-3 my-3' key={element.url}>
-             <Newsitem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,70):""} 
-             imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} 
-             source={element.source.name}
-             />
 
-            </div>
-          })} */}
           {this.state.article && this.state.article.length > 0 ? (
     this.state.article.map((element) => {
         return (
@@ -207,7 +126,6 @@ export class News extends Component {
 ) : (
     <div>No articles available</div>
 )}
-
       </div>
       </div> 
       </InfiniteScroll> 
